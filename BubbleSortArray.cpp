daa8543a52c640e3d54b710a0617530fd1d2c168
kevinnadr <kevin19305.ib@gmail.com>
2024-03-07 21:03:00 +0700

    Bubble Sort Array

--- BubbleSortArray.cpp
+++ BubbleSortArray.cpp
@@ -24,3 +24,26 @@ void input() {   // procedur untuk input
 		cin >> arr[i];
 	}
 }
+
+void BubbleShortArray() { // procedur untuk mengurutkan array dengan metode bubble sort
+	int pass = 1; // step 1
+
+	do {
+		for (int j = 0; j <= n - 1 - pass; j++) {
+			if (arr[j] > arr[j + 1]) {
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+
+		}
+		pass = pass + 1; //step 4
+
+		cout << "\nPass " << pass - 1 << -1 << "; ";   //output ke layar
+		for (int k = 0; k < n; k++) {             //looping nilai k dimulai dari 0 hingga n-1
+			cout << arr[k] << " ";                //output ke layar
+
+		}
+		cout << endl;
+	} while (pass <= n - 1);
+}
