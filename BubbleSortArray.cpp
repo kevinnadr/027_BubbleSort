5f048eff7948415d84be314b71457453cf6ed73c
kevinnadr <kevin19305.ib@gmail.com>
2024-03-07 21:04:56 +0700

    display

--- BubbleSortArray.cpp
+++ BubbleSortArray.cpp
@@ -47,3 +47,21 @@ void BubbleShortArray() { // procedur untuk mengurutkan array dengan metode bubb
 		cout << endl;
 	} while (pass <= n - 1);
 }
+
+void display() {
+	cout << endl;
+	cout << "================================" << endl;
+	cout << "element array yang telah tersusun" << endl;
+	cout << "================================" << endl;
+	cout << endl;
+	for (int j = 0; j < n; j++) {
+		cout << arr[j];
+		if (j < n - 1) {
+			cout << " --> ";
+		}
+	}
+	cout << endl;
+	cout << endl;
+	cout << "Jumlah pass = " << n - 1 << endl;
+	cout << endl;
+}
